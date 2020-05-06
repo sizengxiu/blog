42879176e2cd174fdd817a2aa85974179c2487df
liyi <liyi@123>
2020-05-06 23:05:23 +0800

    初次提交

---
+++ blog/src/main/java/com/si/blog/App.java
@@ -0,0 +1,13 @@
+package com.si.blog;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+        System.out.println("Hello World!");
+    }
+}
