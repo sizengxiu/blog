c4d0a03b2d47eb9409d55dbd1b90a0592649e6b7
liyi <liyi@123>
2020-05-08 22:07:39 +0800

    提交测试

--- blog/src/main/java/com/si/blog/App.java
+++ blog/src/main/java/com/si/blog/App.java
@@ -1,5 +1,8 @@
 package com.si.blog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,6 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
         System.out.println("Hello World!");
+        System.out.println(1);
     }
 }
