15321bf858c34c98af11b18e8dd2f15b8e87c753
liyi <liyi@123>
2020-05-09 21:24:35 +0800

    test

---
+++ blog/target/classes/static/main/js/nav.js
@@ -0,0 +1,63 @@
+$(function(){
+    // nav收缩展开
+    $('.nav-item>a').on('click',function(){
+        if (!$('.nav').hasClass('nav-mini')) {
+            if ($(this).next().css('display') == "none") {
+                //展开未展开
+                $('.nav-item').children('ul').slideUp(300);
+                $(this).next('ul').slideDown(300);
+                $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+            }else{
+                //收缩已展开
+                $(this).next('ul').slideUp(300);
+                $('.nav-item.nav-show').removeClass('nav-show');
+            }
+        }
+    });
+    //nav-mini切换
+    $('#mini').on('click',function(){
+        if (!$('.nav').hasClass('nav-mini')) {
+            $('.nav-item.nav-show').removeClass('nav-show');
+            $('.nav-item').children('ul').removeAttr('style');
+            $('.nav').addClass('nav-mini');
+        }else{
+            $('.nav').removeClass('nav-mini');
+        }
+    });
+
+        $(".nav-item>ul>li>a ").on("click",function() {
+            var address = $(this).attr("data-src");
+            // $("iframe").attr("src", address);
+            $("#content").attr("src", "blog/jvm/test.html");
+        });
+
+
+
+});
+/**
+ * iframe自适应高度，height为手动设置的最小高度
+ */
+function initIframeHeight(height){
+    alert(3);
+    var userAgent = navigator.userAgent;
+    var iframe = parent.document.getElementById("contentIframe");
+    var subdoc = iframe.contentDocument || iframe.contentWindow.document;
+    var subbody = subdoc.body;
+    var realHeight;
+    //谷歌浏览器特殊处理
+    if(userAgent.indexOf("Chrome") > -1){
+        realHeight = subdoc.documentElement.scrollHeight;
+    }
+    else{
+        realHeight = subbody.scrollHeight;
+    }
+    if(realHeight < height){
+        $(iframe).height(height);
+    }
+    else{
+        $(iframe).height(realHeight);
+    }
+}
+// function openPaper(url) {
+//     $("iframe").attr("src", url);
+// }
